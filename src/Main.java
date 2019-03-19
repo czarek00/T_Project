4282e43f6f27a5d9c0961d844e11c87f26f3cadd
czare <czarek00@gmail.com>
2019-03-19 22:52:53 +0100

    third

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     private static WorkC workC = new WorkC();
-
+    private static WorkC2 workC2 = new WorkC2();
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("test1");
-        workC.chech_primes(20);
+     //   System.out.println("test1");
+    //    workC.chech_primes(20);
+        for (int i = 1; i <101 ; i++) {
+            workC2.checkIfPierwsza2(i);
+        }
+
 
     }
 }
